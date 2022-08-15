4dc61ec8ec29ca05b2e68f8a334de82923a04863
Leonel <lelobo75@gmail.com>
2022-08-15 22:02:06 +0200

    Display Community Posts

--- app/Http/Controllers/Frontend/CommunityController.php
+++ app/Http/Controllers/Frontend/CommunityController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use Inertia\Inertia;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CommunityPostResource;
+use App\Http\Resources\CommunityResource;
 use App\Models\Community;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Inertia\Inertia;
 
 class CommunityController extends Controller
 {
     public function show($slug)
     {
         $community = Community::where('slug', $slug)->first();
+        $posts = CommunityPostResource::collection($community->posts()->paginate(12));
+       
+   
 
-        return Inertia::render('Frontend/Communities/Show', compact('community'));
-
+        return Inertia::render('Frontend/Communities/Show', compact('community', 'posts'));
     }
 }
--- resources/js/Pages/Frontend/Communities/Show.vue
+++ resources/js/Pages/Frontend/Communities/Show.vue
@@ -1,12 +1,32 @@
 <template>
 <guest-layout>
+      <template #header>
+      <div class="flex justify-between m-2 p-2">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          r/{{ community.name }}
+        </h2>
+        <Link
+          v-if="$page.props.auth.auth_check"
+          :href="route('communities.posts.create', community.slug)"
+          class="px-3 py-2 rounded bg-indigo-500 hover:bg-indigo-300 text-white"
+          >Create Post</Link
+        >
+      </div>
+    </template>
   <div class="flex justify-end md-2 p-2">
    <Link 
    v-if="$page.props.auth.auth_check" 
    :href="route('communities.posts.create', community.slug)" 
-   class="px-3 py-4 rounded bg-indigo-500 hover:bg-indigo-300 text-white">Create Post</Link>
+   class="px-3 py-4 rounded bg-indigo-500">Create Post</Link>
   </div>
-  <h1>Community {{ community.name }} show page</h1>
+  <section class="m-2 p-2">
+   <PostCard
+          v-for="post in posts.data"
+          :post = "post"
+          :community = "community.name"
+          :key="post.id"
+        />
+  </section>
 </guest-layout>
 
 </template>
@@ -14,8 +34,10 @@
 <script setup>
 import GuestLayout from "@/Layouts/Guest.vue"
 import { Link } from "@inertiajs/inertia-vue3"
+import PostCard from "@/Components/PostCard.vue";
 defineProps({
-    community: Object(),
+    community: Object,
+    posts: Object
 })
 
 </script>

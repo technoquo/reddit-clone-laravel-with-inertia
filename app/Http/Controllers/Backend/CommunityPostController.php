b85cd4a92a010bd00d2c27912e2daf262f74e58f
Leonel <lelobo75@gmail.com>
2022-08-19 11:52:15 +0200

    edit and delete post

--- app/Http/Controllers/Backend/CommunityPostController.php
+++ app/Http/Controllers/Backend/CommunityPostController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\StorePostRequest;
+use App\Models\Post;
+use Inertia\Inertia;
 use App\Models\Community;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redis;
-use Inertia\Inertia;
+use App\Http\Requests\StorePostRequest;
+use Illuminate\Support\Facades\Redirect;
 
 class CommunityPostController extends Controller
 {
@@ -26,6 +27,24 @@ class CommunityPostController extends Controller
             'description' => $request->description
         ]);
 
+        return Redirect::route('frontend.communities.show', $community->slug);
+    }
+
+    public function edit(Community $community, Post $post)
+    {
+        return  Inertia::render('Communities/Posts/Edit', compact('community', 'post'));
+    }
+
+    public function update(StorePostRequest $request, Community $community, Post $post)
+    {
+        
+        $post->update($request->validated());
+        return Redirect::route('frontend.communities.posts.show', [$community->slug, $post->slug] );
+    }
+
+    public function destroy(Community $community, Post $post)
+    {
+        $post->delete();
         return Redirect::route('frontend.communities.show', $community->slug);
     }
 }
